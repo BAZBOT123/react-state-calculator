639be4ee83d331e9e8e117740b05d34feabbeea8
BAZBOT123 <hughesy1993@hotmail.co.uk>
2022-02-24 20:40:46 +0000

    Working on extensions

--- src/App.js
+++ src/App.js
@@ -1,54 +1,107 @@
 import "./App.css"
+import {useState} from 'react'
 
 function App() { 
+  let [number, setNumber] = useState('0')
+  let [digit, setSecNumber] = useState('0')
+  let [operator, setOperator] = useState('+')
+
+ 
+function firstPanel(event) {
+  console.log(event.target.innerText)
+   setNumber((x) => {
+    if(x[0] === '0'){
+      x = ''
+    }
+    return x + event.target.innerText
+  }) 
+}
+
+function secondPanel(event) {
+  console.log(event.target.innerText)
+  setSecNumber((x) => {
+    if(x[0] === '0'){
+      x = ''
+    }
+    return x + event.target.innerText
+  }) 
+  // return setSecNumber(digit + event.target.innerText)
+}
+
+function Calculate(event){
+  return setOperator(event.target.innerText)
+}
+
+let operations = {
+  '+': function (a, b) {
+    return a + b
+  },
+  '-': function (a, b) {
+    return a - b
+  },
+  '*': function (a, b) {
+    return a * b
+  },
+  '÷': function (a, b) {
+    return a / b
+  }
+}
+
+let answer = operations[operator](Number(number),Number(digit))
 
   return (
     <div className="calculator">
         <div className="panel">
-          <p>0</p>
+          <p>{number}</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={firstPanel}>1</button>
+            <button onClick={firstPanel}>2</button>
+            <button onClick={firstPanel}>3</button>
+            <button onClick={firstPanel}>4</button>
+            <button onClick={firstPanel}>5</button>
+            <button onClick={firstPanel}>6</button>
+            <button onClick={firstPanel}>7</button>
+            <button onClick={firstPanel}>8</button>
+            <button onClick={firstPanel}>9</button>
+            <button onClick={firstPanel}>0</button>
+            <button onClick={() => setNumber(number = 0)}>Clear</button>
+            <button onClick={firstPanel}>.</button>
           </div>
         </div>
         
         <div className="panel">
-          <p>+</p>
+          <p>{operator}</p>
           <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
+            <button onClick={Calculate}>+</button>
+            <button onClick={Calculate}>-</button>
+            <button onClick={Calculate}>*</button>
+            <button onClick={Calculate}>÷</button>
           </div>
         </div>
 
         <div className="panel">
-          <p>0</p>
+          <p>{digit}</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={secondPanel}>1</button>
+            <button onClick={secondPanel}>2</button>
+            <button onClick={secondPanel}>3</button>
+            <button onClick={secondPanel}>4</button>
+            <button onClick={secondPanel}>5</button>
+            <button onClick={secondPanel}>6</button>
+            <button onClick={secondPanel}>7</button>
+            <button onClick={secondPanel}>8</button>
+            <button onClick={secondPanel}>9</button>
+            <button onClick={secondPanel}>0</button>
+            <button onClick={() => setSecNumber(digit = 0)}>Clear</button>
+            <button onClick={secondPanel}>.</button>
           </div>
         </div>
         <div className="panel answer">
-          <p>0</p>
+          <p>{answer}</p>
+          <div className="numbers">
+            <button onClick={null}>Store</button>
+            <button onClick={null}>Recall</button>
+            </div>
         </div>
     </div>
   )
